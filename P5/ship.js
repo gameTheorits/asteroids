bc7e8e0662584dccae994a07796a0bbcf944b645
Gametheorist <61074167+gameTheorits@users.noreply.github.com>
2020-06-16 00:14:49 +0530

    Update ship.js

--- P5/ship.js
+++ P5/ship.js
@@ -41,7 +41,7 @@ class Ship {
     translate(this.pos.x, this.pos.y);
     rotate(this.heading + PI / 2);
     fill(this.c);
-    stroke(255);
+    stroke(101,253,6);
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     pop();
   };
